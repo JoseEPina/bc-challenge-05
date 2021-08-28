3b8bc1219af08f624c3b703da22f455a7cc177c8
JoseEPina <jose.edpina@gmail.com>
2021-08-28 17:18:28 -0600

    added ul and one li element

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 var dateElement = document.querySelector("#currentDay");
 var hourElement = document.querySelector("#currentTime");
 
+// Create new elements to display to DOM with bootstrap
+
 var updateClock = function () {
    hourElement.innerHTML = moment().format("h:mm:ss a");
 };
@@ -11,6 +13,38 @@ var startScheduler = function () {
    hourElement.innerHTML = moment().format("h:mm:ss a");
 
    var timerStart = setInterval(updateClock, 1000);
+
+   var containerDiv = document.querySelector(".container");
+   console.log(containerDiv);
+
+   var listGroup = document.createElement("ul");
+   listGroup.className = "day-group";
+   console.log("this is a: " + listGroup);
+
+   var listItems = document.createElement("li");
+   listItems.className = "row day-item";
+   console.log("this will print: " + listItems);
+
+   var taskTimeBlock = document.createElement("span");
+   taskTimeBlock.className = "col-3";
+   taskTimeBlock.textContent = "HOur";
+   var taskDescription = document.createElement("p");
+   taskDescription.className = "col-6";
+   taskDescription.textContent = "Dummy Task";
+   var taskSaveIcon = document.createElement("span");
+   taskSaveIcon.className = "col-3";
+   taskSaveIcon.textContent = "Icon";
+
+   listItems.appendChild(taskTimeBlock);
+   listItems.appendChild(taskDescription);
+   listItems.appendChild(taskSaveIcon);
+   console.log(listItems);
+
+   listGroup.appendChild(listItems);
+   console.log(listGroup);
+
+   containerDiv.appendChild(listGroup);
+   console.log(listGroup);
 };
 
 startScheduler();
