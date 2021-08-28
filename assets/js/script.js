10db7a30440d4f384eb0d7903cd12258fe9ed20c
JoseEPina <jose.edpina@gmail.com>
2021-08-28 17:33:19 -0600

    Added Loop with timeBlock

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,36 +15,34 @@ var startScheduler = function () {
    var timerStart = setInterval(updateClock, 1000);
 
    var containerDiv = document.querySelector(".container");
-   console.log(containerDiv);
 
    var listGroup = document.createElement("ul");
    listGroup.className = "day-group";
-   console.log("this is a: " + listGroup);
-
-   var listItems = document.createElement("li");
-   listItems.className = "row day-item";
-   console.log("this will print: " + listItems);
-
-   var taskTimeBlock = document.createElement("span");
-   taskTimeBlock.className = "col-3";
-   taskTimeBlock.textContent = "HOur";
-   var taskDescription = document.createElement("p");
-   taskDescription.className = "col-6";
-   taskDescription.textContent = "Dummy Task";
-   var taskSaveIcon = document.createElement("span");
-   taskSaveIcon.className = "col-3";
-   taskSaveIcon.textContent = "Icon";
-
-   listItems.appendChild(taskTimeBlock);
-   listItems.appendChild(taskDescription);
-   listItems.appendChild(taskSaveIcon);
-   console.log(listItems);
-
-   listGroup.appendChild(listItems);
-   console.log(listGroup);
-
    containerDiv.appendChild(listGroup);
-   console.log(listGroup);
+
+   const START_TIME = 9;
+   const END_TIME = 18;
+
+   for (var hour = START_TIME; hour < END_TIME; hour++) {
+      var listItems = document.createElement("li");
+      listItems.className = "row day-item";
+
+      var taskTimeBlock = document.createElement("span");
+      taskTimeBlock.className = "col-3";
+      taskTimeBlock.textContent = "HOur";
+      var taskDescription = document.createElement("p");
+      taskDescription.className = "col-6";
+      taskDescription.textContent = "Dummy Task";
+      var taskSaveIcon = document.createElement("span");
+      taskSaveIcon.className = "col-3";
+      taskSaveIcon.textContent = "Icon";
+
+      listItems.appendChild(taskTimeBlock);
+      listItems.appendChild(taskDescription);
+      listItems.appendChild(taskSaveIcon);
+
+      listGroup.appendChild(listItems);
+   }
 };
 
 startScheduler();
